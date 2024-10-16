a6da13415f3be350d6a014972a722388dce2f60b
mherferd <mherferd@g.educaand.es>
2024-10-16 22:26:12 +0200

    antes de compartido

---
+++ TEMA 2/Boletin 7.3/Ejercicio 4.php
@@ -0,0 +1,31 @@
+<?php
+
+echo "<h1>Ejercicio 4 -- Boletin 7.3</h1><br>";
+
+//Creo el array de resultados
+$resultados = [];
+
+
+
+//Genera las imágenes de los dados y añado los resultados al array
+echo "<p>Tirada de 6 dados</p>";
+
+for ($i = 1; $i <= 6; $i++) {
+    $dado = rand(1,6);
+    echo "<img src='imagenes/$dado.svg'> ";
+    $resultados[] = $dado;
+}
+
+
+
+//Genero un dado aleatorio para eliminar de los resultados
+echo "<p>Dado a eliminar:</p>";
+
+$dado_eliminar = rand(1,6);
+echo "<img src='imagenes/$dado.svg'> ";
+
+
+
+//Elimino del array de resultados el dado aleatorio y muestro los resultados restantes
+
+?>
