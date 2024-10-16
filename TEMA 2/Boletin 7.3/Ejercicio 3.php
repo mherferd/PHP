061cac7f28b0b6d78fe8eaed1c05d02ebd4dd1f7
mherferd <mherferd@g.educaand.es>
2024-10-16 22:15:26 +0200

    Termino 3 ejercicios

--- TEMA 2/Boletin 7.3/Ejercicio 3.php
+++ TEMA 2/Boletin 7.3/Ejercicio 3.php
@@ -2,13 +2,18 @@
 
 echo "<h1>Ejercicio 3 -- Boletin 7.3</h1><br>";
 
+//Creo variables para contar los ganadores y los empates.
 $ganador_1 = 0;
 $ganador_2 = 0;
 $empates = 0;
 
+
+//Creo arrays para guardar los resultados de cada jugador.
 $Resultados_1 = [];
 $Resultados_2 = [];
 
+
+//Simulo los dados para el jugador 1 y los guardo en arrays.
 echo "<p>Jugador 1:</p>";
 
 for ($i = 1; $i <= 4; $i++) {
@@ -17,6 +22,8 @@ for ($i = 1; $i <= 4; $i++) {
     $Resultados_1[$i] = $dado1;
 }
 
+
+//Simulo los dados para el jugador 1 y los guardo en arrays.
 echo "<p>Jugador 2:</p>";
 
 for ($i = 1; $i <= 4; $i++) {
@@ -25,6 +32,8 @@ for ($i = 1; $i <= 4; $i++) {
     $Resultados_2[$i] = $dado2;
 }
 
+
+//Comparo los resultados de cada jugador para contar los ganadores. 
 for ($i = 1; $i <= 4; $i++){
     if($Resultados_1[$i] < $Resultados_2[$i]){
         $ganador_2++;
@@ -39,6 +48,8 @@ for ($i = 1; $i <= 4; $i++){
 
 echo "<p>El jugador 1 ha ganado $ganador_1 veces, el jugador 2 ha ganado $ganador_2 veces, y han empatado $empates veces</p>";
 
+
+//Comparo los ganadores para determinar quien ha ganado.
 if ($ganador_1<$ganador_2){
     echo "<p>Ha ganado el jugador 2</p>";
 }
