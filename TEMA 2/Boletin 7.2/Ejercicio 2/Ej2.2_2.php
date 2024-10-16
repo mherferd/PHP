d606836cbe2946c0d2e6b57c6d077797858ea96b
mherferd <mherferd@g.educaand.es>
2024-10-16 14:32:04 +0200

    Termino ejercicio 7.2

--- TEMA 2/Boletin 7.2/Ejercicio 2/Ej2.2_2.php
+++ TEMA 2/Boletin 7.2/Ejercicio 2/Ej2.2_2.php
@@ -1,22 +1,125 @@
 <?php
 
-//Imprimimos los datos que nos llegan del formulario del documento .php anterior
-echo "<p>Tus datos originales son: </p>";
-foreach ($_REQUEST as $contador){
-    echo $contador;
+// Recibimos los valores de la fila, la columna, el tamaño de la matriz y la direccion seleccionadas
+$fila_seleccionada = $_POST['fila'];
+$columna_seleccionada = $_POST['columna'];
+$direccion_seleccionada = $_POST['direccion'];
+
+if (isset($_POST['valor'])) {
+    $valor = $_POST['valor'];
+} 
+
+// Restamos 1 para alinear con los índices de las celdas
+$fila = $fila_seleccionada - 1;
+$columna = $columna_seleccionada - 1;
+
+$valores_celdas = [];
+
+//Extraemos los valores segun la direccion
+switch ($direccion_seleccionada) {
+
+    case 'ABDE': 
+        for ($i = 0; $i < $valor; $i++) {
+            if ($fila + $i < $valor && $columna + $i < $valor) { 
+                $celda = "celda" . ($fila + $i) . "_" . ($columna + $i);
+                if (isset($_POST[$celda])) {
+                    $valores_celdas[] = $_POST[$celda]; 
+                }
+            }
+        }
+        break;
+
+    case 'ABIZ': 
+        for ($i = 0; $i < $valor; $i++) {
+            if ($fila + $i < $valor && $columna - $i >= 0) { 
+                $celda = "celda" . ($fila + $i) . "_" . ($columna - $i);
+                if (isset($_POST[$celda])) {
+                    $valores_celdas[] = $_POST[$celda]; 
+                }
+            }
+        }
+        break;
+
+    case 'ARDE': 
+        for ($i = 0; $i < $valor; $i++) {
+            if ($fila - $i >= 0 && $columna + $i < $valor) { 
+                $celda = "celda" . ($fila - $i) . "_" . ($columna + $i);
+                if (isset($_POST[$celda])) {
+                    $valores_celdas[] = $_POST[$celda]; 
+                }
+            }
+        }
+        break;
+
+    case 'ARIZ': 
+        for ($i = 0; $i < $valor; $i++) {
+            if ($fila - $i >= 0 && $columna - $i >= 0) { 
+                $celda = "celda" . ($fila - $i) . "_" . ($columna - $i);
+                if (isset($_POST[$celda])) {
+                    $valores_celdas[] = $_POST[$celda]; 
+                }
+            }
+        }
+        break;
+
+        case 'AR': 
+            for ($i = $fila; $i >=0; $i--) {
+                $celda = "celda" . ($fila-$i) . "_" . ($columna);
+                    if (isset($_POST[$celda])) {
+                        $valores_celdas[] = $_POST[$celda]; 
+                    }
+                
+                }
+            break;
+
+            case 'AB': 
+                for ($i = $fila; $i < $valor; $i++) {
+                    $celda = "celda" . ($fila+$i) . "_" . ($columna);
+                        if (isset($_POST[$celda])) {
+                            $valores_celdas[] = $_POST[$celda]; 
+                        }
+                    
+                    }
+                break;
+
+            case 'IZ': 
+                for ($i = $columna; $i >=0; $i--) {
+                    $celda = "celda" . ($fila) . "_" . ($columna-$i);
+                        if (isset($_POST[$celda])) {
+                            $valores_celdas[] = $_POST[$celda]; 
+                        }
+                    
+                    }
+                break;
+
+                case 'DE': 
+                    for ($i = $columna; $i<$valor   ; $i++) {
+                        $celda = "celda" . ($fila) . "_" . ($columna+$i);
+                            if (isset($_POST[$celda])) {
+                                $valores_celdas[] = $_POST[$celda]; 
+                            }
+                        
+                        }
+                    break;
+
+        
+
+    default:
+        echo "Dirección no válida.";
 }
 
-
-//Y ahora imprimimos los datos invertidos
-echo "<br><br>Tus datos invertidos son: <br>";
-
-$array_delreves = array_reverse($_REQUEST);
-echo "<br>";
-
-foreach ($array_delreves as $reverso){
-    echo $reverso;
+// Imprimimos los valores de las celdas obtenidas
+if (!empty($valores_celdas)) {
+    echo "Valores de la matriz partiendo de la posicion ($fila_seleccionada , $columna_seleccionada) y en la dirección seleccionada es:<br>";
+    foreach ($valores_celdas as $valor_celda) {
+        echo "$valor_celda<br>";
+    }
+} else {
+    echo "No se encontraron valores en la dirección '$direccion_seleccionada'.";
 }
+
 
 echo "<br><br>";
-echo"<a href=Ej7.2.html>Volver al inicio</a>";
+echo "<a href='Ej7.2.html'>Volver al inicio</a>";
+
 ?>
--- TEMA 2/Boletin 7.2/Ejercicio 2/Ej7_2.php
+++ TEMA 2/Boletin 7.2/Ejercicio 2/Ej7_2.php
@@ -2,62 +2,64 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $valor = $_POST['valor'];
 
-    //Verificamos si está entre 2 y 10
-        if ($valor<=10 && $valor>=2) {
-            echo "<p>Numero de elementos: '$valor' x '$valor'</p>";
-            echo "<label for='valor2'>Introduce los elementos a tratar:</label><br><br>";
+    // Verificamos si está entre 2 y 10
+    if ($valor <= 10 && $valor >= 2) {
+        echo "<p>Numero de elementos: '$valor' x '$valor'</p>";
+        echo "<label for='valor2'>Introduce los elementos a tratar:</label><br><br>";
 
-            echo "<form action='Ej2.2_2.php' method='post'>";
-            //Creamos un bucle for para crear tantos inputs según el número de elementos introducidos
-            for ($i=0; $i<$valor; $i++) {
-                echo "<br>";
-                for ($j=0; $j<$valor; $j++) {
-                echo "<input type='text' id='celda$i+$j' name='celda$i+$j' size=3 required>";
+        echo "<form action='Ej2.2_2.php' method='post'>";
+
+        // Campo oculto que envía el valor de la matriz
+        echo "<input type='hidden' name='valor' value='$valor'>";
+
+
+        // Creamos un bucle for para crear tantos inputs según el número de elementos introducidos
+        for ($i = 0; $i < $valor; $i++) {
+            echo "<br>";
+            for ($j = 0; $j < $valor; $j++) {
+                echo "<input type='text' id='celda{$i}_{$j}' name='celda{$i}_{$j}' size='3' required>";
             }
         }
 
 
-            //Una vez creados los inputs, mostramos los botones de seleccionar elemento mediante bucles for dependiendo el tamaño de la matriz
-            echo "<br><br>";
-            echo "<label for='fila'>Selecciona una fila:</label>";
-            echo "<select id='fila' name='fila'>";
-                for ($i=0; $i<$valor; $i++) {
-                echo "<option value='$i'>$i</option>";
-               
-            }
-            echo"</select><br><br>";
-
-
-            echo "<label for='columna'>Selecciona una columna:</label>";
-            echo "<select id='columna' name='columna'>";
-
-            for ($i=0; $i<$valor; $i++) {
+        // Creamos los inputs para seleccionar la fila y columna y la dirección
+        echo "<br><br>";
+        echo "<label for='fila'>Selecciona una fila:</label>";
+        echo "<select id='fila' name='fila'>";
+        for ($i = 1; $i <= $valor; $i++) {
             echo "<option value='$i'>$i</option>";
-           
-            }
-            echo"</select><br><br>";
-
-
-            echo "<label for='direccion'>Introduzca una trayectoria:</label>";
-            echo "<select id='direccion' name='direccion'>";
-                echo "<option value='ARDE'>Arriba-Derecha</option>";
-                echo "<option value='ARIZ'>Arriba-Izquierda</option>";
-                echo "<option value='ABDE'>Abajo-Derecha</option>";
-                echo "<option value='ABIZ'>Abajo-Izquierda</option>";
-            echo"</select>";
-
-            //Mostramos el botón de enviar y borrar y un enlace para volver al inicio
-            echo "<br><br>";
-            echo "<button type='submit' value='Enviar'>Enviar</button>";
-            echo "<button type='reset' value='Borrar'>Borrar</button><br><br>";
-            echo"<a href=Ej7.2.html>Volver al inicio</a>";
-            echo "</form>";
-
         }
-        else{   
-            echo "<p>Error, el valor '$valor' no está en el rango de 2 a 10.</p>";
-            echo"<a href=Ej7.2.html>Volver al inicio</a>";
+        echo "</select><br><br>";
+
+        echo "<label for='columna'>Selecciona una columna:</label>";
+        echo "<select id='columna' name='columna'>";
+        for ($i = 1; $i <= $valor; $i++) {
+            echo "<option value='$i'>$i</option>";
         }
+        echo "</select><br><br>";
+
+        echo "<label for='direccion'>Introduzca una trayectoria:</label>";
+        echo "<select id='direccion' name='direccion'>";
+        echo "<option value='AR'>Arriba</option>";
+        echo "<option value='AB'>Abajo</option>";
+        echo "<option value='DE'>Derecha</option>";
+        echo "<option value='IZ'>Izquierda</option>";
+        echo "<option value='ARDE'>Arriba-Derecha</option>";
+        echo "<option value='ARIZ'>Arriba-Izquierda</option>";
+        echo "<option value='ABDE'>Abajo-Derecha</option>";
+        echo "<option value='ABIZ'>Abajo-Izquierda</option>";
+        echo "</select>";
+
+        // Botones de enviar y borrar
+        echo "<br><br>";
+        echo "<button type='submit' value='Enviar'>Enviar</button>";
+        echo "<button type='reset' value='Borrar'>Borrar</button><br><br>";
+        echo "<a href='Ej7.2.html'>Volver al inicio</a>";
+        echo "</form>";
+
+    } else {   
+        echo "<p>Error, el valor '$valor' no está en el rango de 2 a 10.</p>";
+        echo "<a href='Ej7.2.html'>Volver al inicio</a>";
     }
-
+}
 ?>
